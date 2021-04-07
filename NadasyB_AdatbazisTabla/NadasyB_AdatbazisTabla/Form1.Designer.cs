f5ec5a04a7ad4eb4de89d722cc681076cf455b73
NadasyB <nadasy.bendeguz@gmail.com>
2021-04-07 15:05:38 +0200

    Program félkész

---
+++ NadasyB_AdatbazisTabla/NadasyB_AdatbazisTabla/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace NadasyB_AdatbazisTabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.txtTorol = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(449, 259);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Autók listája:";
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(674, 55);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(75, 23);
+            this.btnTorol.TabIndex = 2;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // txtTorol
+            // 
+            this.txtTorol.Location = new System.Drawing.Point(640, 55);
+            this.txtTorol.Name = "txtTorol";
+            this.txtTorol.Size = new System.Drawing.Size(28, 22);
+            this.txtTorol.TabIndex = 3;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(136, 26);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Törlendő sorszáma:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtTorol);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.TextBox txtTorol;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NadasyB_AdatbazisTabla/NadasyB_AdatbazisTabla/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NadasyB_AdatbazisTabla
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Auto> autokLista;
+        DataTable raktarTable;
+        public Form1()
+        {
+            InitializeComponent();
+            ini();
+
+        }
+
+        private void ini()
+        {
+            autokLista = new List<Auto>();
+            autokLista.Add(new Auto("Pál", "BMW", "zöld"));
+            autokLista.Add(new Auto("Petra", "Opel", "kék"));
+            autokLista.Add(new Auto("Gábor", "Toyota", "fehér"));
+            autokLista.Add(new Auto("Péter", "BMW", "ezüst"));
+
+            raktarTable = new DataTable();
+            tablaKeszit();
+            tablaTolt();
+
+            //megjelenít
+            dataGridView1.DataSource = raktarTable;
+        }
+
+        private void tablaTolt()
+        {
+            foreach (Auto auto in autokLista)
+            {
+                DataRow dr = raktarTable.NewRow();
+                dr["Gyártó"] = auto.Gyarto;
+                dr["Szín"] = auto.Szin;
+                dr["Név"] = auto.Nev;
+                
+                raktarTable.Rows.Add(dr);
+            }
+        }
+
+        private void tablaKeszit()
+        {
+            DataColumn autoldColumn = new DataColumn("Autó ID", typeof(int));
+            autoldColumn.Caption = "Autó ID";
+            autoldColumn.ReadOnly = true;
+            autoldColumn.AllowDBNull = false;
+            autoldColumn.Unique = true;
+            autoldColumn.AutoIncrement = true;
+            autoldColumn.AutoIncrementSeed = 1;
+            autoldColumn.AutoIncrementStep = 1;
+
+            DataColumn gyartoColumn = new DataColumn("Gyártó", typeof(string));
+            DataColumn szinColumn = new DataColumn("Szín", typeof(string));
+            DataColumn nevColumn = new DataColumn("Név", typeof(string));
+            //autoNevColumn.Caption
+
+            raktarTable.Columns.AddRange(new DataColumn[] {
+                autoldColumn,
+                gyartoColumn,
+                szinColumn,
+                nevColumn
+            });
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                raktarTable.Rows[int.Parse(txtTorol.Text)-1].Delete();
+                raktarTable.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ini();
+        }
+    }
+}
